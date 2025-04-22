e5733bcb8def584f49cd7b384a4ae6a412c1ca1c
gagaprince <wangzidongprince@163.com>
2025-04-23 01:17:17 +0800

    feat: 新增 flux

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import { batch_clothes_task, animeTaskForForest, peopleLoraTaskForForest, mov2animeTask, mov2animePreview, beginBatchTask, beginInterrogate } from './tasks'
+import { beginFluxBatchTask,beginInterrogateFlux, batch_clothes_task, animeTaskForForest, peopleLoraTaskForForest, mov2animeTask, mov2animePreview, beginBatchTask, beginInterrogate } from './tasks'
+
+
+async function sleep(time){
+    return new Promise((res)=>{
+        setTimeout(()=>{
+            res();
+        },time)
+    })
+}
 
 async function main(){
     const movPath = 'G:\\webui\\videowork\\v2v\\20240924\\'
@@ -7,15 +16,35 @@ async function main(){
     // const imgpath = '/Users/gagaprince/Downloads/xiaozhangyu/xiaozhangyu-0003.png';
     // mov2animePreview(imgpath);
 
-    // beginBatchTask(); 
-    // beginInterrogate('liujiayi');
-    // beginInterrogate('meiya');
-    // beginInterrogate('qingjiujiu');
-    // beginInterrogate('erguotou');
-    // beginInterrogate('huliena');
-    // beginInterrogate('xingxing');
-    // beginInterrogate('immy');
-    // beginInterrogate('roulaoban');
+    const list = 'juemei,77,7ge,wansu,zhendehen,zhenzhendawang,zhiyu'.split(',');
+
+    // beginBatchTask(list,'20250418'); 
+    // beginFluxBatchTask(list,'20250418'); 
+
+    // beginInterrogate('77');
+    beginInterrogateFlux('bingqian', true);
+
+    // for(let i=0;i<list.length;i++){
+    //     const key = list[i];
+    //     let flag = false;
+    //     if(i===list.length-1){
+    //         flag = true;
+    //     }
+    //     beginInterrogate(key, flag);
+    //     await sleep(4000);
+    // }
+
+
+    
+    // beginInterrogate('xiexin');
+    // beginInterrogate('jiaze');
+    // beginInterrogate('saykiy');
+    // beginInterrogate('chiwudun');
+    // beginInterrogate('buheniwan');
+    // beginInterrogate('wananxiaoyang');
+    // beginInterrogate('yizhibeika');
+    // beginInterrogate('pangpang');
+    // beginInterrogate('yudaG', true);
 
     // beginInterrogate('xiaoxiaozhu');
     // beginInterrogate('luyanqi');
@@ -25,7 +54,7 @@ async function main(){
     // beginInterrogate('jiayi');
 
     // beginInterrogate('xiaoyan');
-    peopleLoraTaskForForest();
+    // peopleLoraTaskForForest();
 }
 
 main();
--- tasks/batchTrainTask.js
+++ tasks/batchTrainTask.js
@@ -55,95 +55,98 @@ export const giveMeTaskOpt = (trainDir, outputName = "test_pony")=>{
     }
 }
 
+export const giveMeFluxTaskOpt = (trainDir, outputName = "test_pony")=>{
+    return {
+        "model_train_type": "flux-lora",
+        "pretrained_model_name_or_path": "G:/webui/sd-webui-aki/ComfyUI-aki-v1.4/models/unet/flux1-dev.safetensors",
+        "ae": "G:/webui/sd-webui-aki/ComfyUI-aki-v1.4/models/vae/ae.safetensors",
+        "clip_l": "G:/webui/sd-webui-aki/ComfyUI-aki-v1.4/models/clip/clip_l.safetensors",
+        "t5xxl": "G:/webui/sd-webui-aki/ComfyUI-aki-v1.4/models/clip/t5xxl_fp16.safetensors",
+        "timestep_sampling": "sigmoid",
+        "sigmoid_scale": 1,
+        "model_prediction_type": "raw",
+        "discrete_flow_shift": 1,
+        "loss_type": "l2",
+        "guidance_scale": 1,
+        "train_t5xxl": false,
+        "train_data_dir": trainDir,//"G:/webui/sd-webui-aki/lora-scripts-v1.10.0/train/bingqian/train",
+        "prior_loss_weight": 1,
+        "resolution": "512,512",
+        "enable_bucket": false,
+        "min_bucket_reso": 256,
+        "max_bucket_reso": 2048,
+        "bucket_reso_steps": 64,
+        "bucket_no_upscale": true,
+        "output_name": outputName,
+        "output_dir": "./output",
+        "save_model_as": "safetensors",
+        "save_precision": "fp16",
+        "save_every_n_epochs": 1,
+        "save_state": false,
+        "max_train_epochs": 9,
+        "train_batch_size": 1,
+        "gradient_checkpointing": true,
+        "gradient_accumulation_steps": 1,
+        "network_train_unet_only": true,
+        "network_train_text_encoder_only": false,
+        "learning_rate": 0.0001,
+        "unet_lr": 0.0005,
+        "text_encoder_lr": 0.00002,
+        "lr_scheduler": "cosine_with_restarts",
+        "lr_warmup_steps": 0,
+        "lr_scheduler_num_cycles": 1,
+        "optimizer_type": "AdamW8bit",
+        "network_module": "networks.lora_flux",
+        "network_dim": 16,
+        "network_alpha": 8,
+        "log_with": "tensorboard",
+        "logging_dir": "./logs",
+        "caption_extension": ".txt",
+        "shuffle_caption": false,
+        "keep_tokens": 0,
+        "seed": 1337,
+        "clip_skip": 2,
+        "mixed_precision": "bf16",
+        "sdpa": true,
+        "lowram": false,
+        "cache_latents": true,
+        "cache_latents_to_disk": true,
+        "cache_text_encoder_outputs": true,
+        "cache_text_encoder_outputs_to_disk": true,
+        "persistent_data_loader_workers": true,
+        "fp8_base": true
+    }
+}
 
-export const beginBatchTask = ()=>{
-    const trainList = [
-        // {
-        //     dirKey: 'shuibingyue',
-        //     outName: 'shuibingyue_pony',
-        // },
-        // {
-        //     dirKey: 'xiaowusu',
-        //     outName: 'xiaowusu_pony',
 
-        // },
-        // {
-        //     dirKey: 'dongmengmeng',
-        //     outName: 'badanmu_pony',
-        // },
-        // {
-        //     dirKey: 'dongmengmeng',
-        //     outName: '20241116_dongmengmeng_pony',
-        // },
-        // {
-        //     dirKey: 'duohedounai',
-        //     outName: '20241116_duohedounai_pony',
-        // },
-        // {
-        //     dirKey: 'huzhien',
-        //     outName: '20241117_huzhien_pony',
-        // },
-        // {
-        //     dirKey: 'xiaohuang',
-        //     outName: '20241117_xiaohuang_pony',
-        // },
-        // {   
-        //     dirKey: 'wangqiaochu',
-        //     outName: '20250122_wangqiaochu_pony',
-        // },
-        // {
-        //     dirKey: 'zhouhongdou',
-        //     outName: '20250129_zhouhongdou_pony',
-        // },
-        // {
-        //     dirKey: 'yiiin',
-        //     outName: '20250208_yiiin_pony',
-        // },
-        // {
-        //     dirKey: 'lebaoei',
-        //     outName: '20250208_lebaoei_pony',
-        // },
-        // {
-        //     dirKey: 'yuyexiaoxuan',
-        //     outName: '20250208_yuyexiaoxuan_pony',
-        // },
-        // {
-        //     dirKey: 'lldmv',
-        //     outName: '20250208_lldmv_pony',
-        // },
-        // {
-        //     dirKey: 'xingxing',
-        //     outName: '20250208_xingxing_pony',
-        // },
-        // {
-        //     dirKey: 'liujiayi',
-        //     outName: '20250228_liujiayi_pony',
-        // },
-        {
-            dirKey: 'meiya',
-            outName: '20250228_meiya_pony',
-        },
-        {
-            dirKey: 'qingjiujiu',
-            outName: '20250228_qingjiujiu_pony',
-        },
-        {
-            dirKey: 'erguotou',
-            outName: '20250228_erguotou_pony',
-        },
-        {
-            dirKey: 'huliena',
-            outName: '20250228_huliena_pony',
-        },
-        // {
-        //     dirKey: 'liaoliao',
-        //     outName: '20250105_liaoliao_pony',
-        // },
-        // {
-        //     dirKey: 'jiayi',
-        //     outName: '20250105_jiayi_pony',
-        // }
-    ]
+export const beginFluxBatchTask = (list, date)=>{
+    const trainList = list.map(item=>({
+        dirKey: item,
+        outName: `${date}_${item}_flux`
+    }))
+
+    //  G:/webui/sd-webui-aki/lora-scripts-v1.10.0/train/${trainOpt.dirKey}/train
+    const options = trainList.map(trainOpt => giveMeFluxTaskOpt(`G:/webui/sd-webui-aki/lora-scripts-v1.10.0/train/${trainOpt.dirKey}/train`, trainOpt.outName))
+    console.log(options);
+    run_batch_task(options)
+}
+
+
+export const beginBatchTask = (list, date)=>{
+    const trainList = list.map(item=>({
+        dirKey: item,
+        outName: `${date}_${item}_pony`
+    }))
+    // const trainList = [
+    //     // {
+    //     //     dirKey: 'buheniwan',
+    //     //     outName: '20250315_buheniwan_pony',
+    //     // },
+    //     // {
+    //     //     dirKey: 'yizhibeika',
+    //     //     outName: '20250315_yizhibeika_pony',
+    //     // },
+    // ]
 
 
     const options = trainList.map(trainOpt => giveMeTaskOpt(`G:/webui/sd-webui-aki/lora-scripts-v1.7.3/train/${trainOpt.dirKey}/train`, trainOpt.outName))
@@ -151,7 +154,7 @@ export const beginBatchTask = ()=>{
     run_batch_task(options)
 }
 
-export const giveMeInterrogateOpt = (key) => {
+export const giveMeInterrogateOpt = (key, unloadModel=false) => {
     return {
         "path": `G:/webui/sd-webui-aki/lora-scripts-v1.7.3/train/${key}/train/20_${key}`,
         "threshold": 0.35,
@@ -159,16 +162,53 @@ export const giveMeInterrogateOpt = (key) => {
         "replace_underscore": true,
         "escape_tag": true,
         "batch_output_action_on_conflict": "copy",
-        "additional_tags": key
+        "additional_tags": key,
+        "unload_model_after_running": unloadModel,
     }
 }
 
-export const beginInterrogate = async (key)=>{
+export const giveMeInterrogateFluxOpt = (key, unloadModel=false) => {
+    return {
+        "path": `G:/webui/sd-webui-aki/lora-scripts-v1.10.0/train/${key}/train/4_${key}`,
+        "threshold": 0.35,
+        "interrogator_model": "wd-vit-v3",
+        "replace_underscore": true,
+        "escape_tag": true,
+        "batch_output_action_on_conflict": "copy",
+        "additional_tags": key,
+        "unload_model_after_running": unloadModel,
+    }
+}
+
+export const beginInterrogate = async (key, unloadModel)=>{
     // const list = ['xiaoshiqi','xiaoyishimei','yeyuanna','yizhimianyang'];
     // const list = ['xiaoyishimei']
     // const optionList = list.map(key => giveMeInterrogateOpt(key));
     // for(let i=0;i<optionList.length; i++){
     //     run_interrogate_task(optionList[i]);
     // }
-    run_interrogate_task(giveMeInterrogateOpt(key))
+    run_interrogate_task(giveMeInterrogateOpt(key, unloadModel))
 }
+
+export const beginInterrogateFlux = async (key, unloadModel)=>{
+    // const list = ['xiaoshiqi','xiaoyishimei','yeyuanna','yizhimianyang'];
+    // const list = ['xiaoyishimei']
+    // const optionList = list.map(key => giveMeInterrogateOpt(key));
+    // for(let i=0;i<optionList.length; i++){
+    //     run_interrogate_task(optionList[i]);
+    // }
+    run_interrogate_task(giveMeInterrogateFluxOpt(key, unloadModel))
+}
+
+export const beginInterrogateBatch = async(key)=>{
+    run_interrogate_task({
+        "path": `G:/webui/sd-webui-aki/lora-scripts-v1.7.3/train/${key}/train/20_${key}`,
+        "threshold": 0.35,
+        "interrogator_model": "wd14-convnextv2-v2",
+        "replace_underscore": true,
+        "escape_tag": true,
+        "batch_output_action_on_conflict": "copy",
+        "additional_tags": key
+    })
+}
+
